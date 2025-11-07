b94532a7a7c1132379696e3730d305ed3023d0b4
Githua Josiah <g.lectricals@gmail.com>
2025-11-07 21:56:00 +0000

    short summary of changes

---
+++ index.js
@@ -0,0 +1,12 @@
+// class working
+console.log("Hello World!");
+// Assignment: show name, age and favourite coding language
+const person = {
+  name: "Josiah Mwangi",
+  age: "10",                    // AI doesn't have a real age; replace with a number if needed
+  favouriteLanguage: "JavaScript"
+};
+console.log("Name: " + person.name);
+console.log("Age: " + person.age);
+console.log("Favourite coding language: " + person.favouriteLanguage);
+
